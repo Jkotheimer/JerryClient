d86582ef74e6fa04fdcb1cbb09224f8762c1c7e1
jwk <jwk@pop-os.localdomain>
2022-11-19 00:41:33 -0600

    Updated ESP8266 server to use websockets for a more responsive system. Updated front end to use the web socket

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,87 +1,87 @@
-#define pinA D5
-#define pinB D6
-#define pinC D7
-#define pinD D8
 
-const int stepsPerRevolution = 700;
+#ifndef LEFT
 
-const int stepSequence[8][4] = {
-  {1,0,0,0},
-  {1,1,0,0},
-  {0,1,0,0},
-  {0,1,1,0},
-  {0,0,1,0},
-  {0,0,1,1},
-  {0,0,0,1},
-  {1,0,0,1}
+#define LEFT "left"
+#define RIGHT "right"
+#define FORWARD 1
+#define REVERSE -1
+#define STOP 0
+
+// Motor pin definitions
+#define LM_P1 D0
+#define LM_P2 D1
+#define LM_P3 D2
+#define LM_P4 D3
+
+#define RM_P1 D7
+#define RM_P2 D8
+#define RM_P3 D9
+#define RM_P4 D10
+
+#define STEP_DELAY 3
+
+#endif
+
+const uint8_t stepSequence[32] = {
+  1,0,0,0,
+  1,1,0,0,
+  0,1,0,0,
+  0,1,1,0,
+  0,0,1,0,
+  0,0,1,1,
+  0,0,0,1,
+  1,0,0,1
 };
 
 void setup() {
-  pinMode(pinA, OUTPUT);
-  pinMode(pinB, OUTPUT);
-  pinMode(pinC, OUTPUT);
-  pinMode(pinD, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("START");
+  
+  pinMode(LM_P1, OUTPUT);
+  pinMode(LM_P2, OUTPUT);
+  pinMode(LM_P3, OUTPUT);
+  pinMode(LM_P4, OUTPUT);
+  pinMode(RM_P1, OUTPUT);
+  pinMode(RM_P2, OUTPUT);
+  pinMode(RM_P3, OUTPUT);
+  pinMode(RM_P4, OUTPUT);
 }
 
-void loop() {
-  step(100, "forward");
-  delay(1000);
-  step(100, "back");
-  delay(1000);
+void loop() {}
+
+void setMotorState(String side, int state) {
+  Serial.print("Setting motor state: ");
+  Serial.print(side);
+  Serial.println(state);
+  if (state == FORWARD) clockwiseStep(side);
+  else if (state == REVERSE) ctrclockwiseStep(side);
 }
 
-void write(int a, int b, int c, int d){
-  digitalWrite(pinA,a);
-  digitalWrite(pinB,b);
-  digitalWrite(pinC,c);
-  digitalWrite(pinD,d);
-}
 
-void step(int steps, char* direction) {
-  if (steps <= 0) return;
-  bool forward = direction == "forward";
-  for (int stepCount = 0; stepCount >= steps; stepCount++) {
-    if (forward) stepForward();
-    else stepBack();
+void clockwiseStep(String side) {
+  for (int8_t i = 0; i < sizeof(stepSequence);) {
+    writeMotorPins(side, stepSequence[i++], stepSequence[i++], stepSequence[i++], stepSequence[i++]);
+    delay(STEP_DELAY);
   }
 }
- 
-void stepForward() {
-  write(1,0,0,0);
-  delay(5);
-  write(1,1,0,0);
-  delay(5);
-  write(0,1,0,0);
-  delay(5);
-  write(0,1,1,0);
-  delay(5);
-  write(0,0,1,0);
-  delay(5);
-  write(0,0,1,1);
-  delay(5);
-  write(0,0,0,1);
-  delay(5);
-  write(1,0,0,1);
-  delay(5);
-  write(1,0,0,0);
+
+void ctrclockwiseStep(String side) {
+  for (int8_t i = sizeof(stepSequence); i > -1; i-=4) {
+    writeMotorPins(side, stepSequence[i-3], stepSequence[i-2], stepSequence[i-1], stepSequence[i]);
+    delay(STEP_DELAY);
+  }
 }
 
-void stepBack() {
-  write(1,0,0,0);
-  delay(5);
-  write(1,0,0,1);
-  delay(5);
-  write(0,0,0,1);
-  delay(5);
-  write(0,0,1,1);
-  delay(5);
-  write(0,0,1,0);
-  delay(5);
-  write(0,1,1,0);
-  delay(5);
-  write(0,1,0,0);
-  delay(5);
-  write(1,1,0,0);
-  delay(5);
-  write(1,0,0,0);
+void writeMotorPins(String side, int a, int b, int c, int d) {
+  if (side == LEFT) {
+    digitalWrite(LM_P1, a);
+    digitalWrite(LM_P2 ,b);
+    digitalWrite(LM_P3, c);
+    digitalWrite(LM_P4, d);
+  } else if (side == RIGHT) {
+    digitalWrite(RM_P1, a);
+    digitalWrite(RM_P2 ,b);
+    digitalWrite(RM_P3, c);
+    digitalWrite(RM_P4, d);
+  }
 }
--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,126 +1,80 @@
-//#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
+#include <WebSocketsServer.h>
 
-//WiFiServer server(80);
+#ifndef STEP_DELAY
 
+// These are the indexes of the respective motor's state in the payload string (which will only be 2 bytes long)
+#define LEFT 0
+#define RIGHT 1
+
+// The websocket payload sends state as a char '0', '1', or '2' for each motor. These are the ascii values.
+#define REVERSE 48 // '0'
+#define STOP 49    // '1'
+#define FORWARD 50 // '2'
+
+#define STEP_DELAY 2
+
+#define CONNECT_INTERVAL 1000
+#define CONNECT_TIMEOUT 5000
+
+#endif
+
+// MOTOR VARIABLES
+const uint8_t rightMotorPins[4] = { D5, D6, D7, D8 };
+const uint8_t leftMotorPins[4] = { D0, D1, D2, D3 };
+const uint8_t stepSequence[32] = {
+  1,0,0,0,
+  1,1,0,0,
+  0,1,0,0,
+  0,1,1,0,
+  0,0,1,0,
+  0,0,1,1,
+  0,0,0,1,
+  1,0,0,1
+};
+
+int8_t leftMotorState = STOP;
+int8_t rightMotorState = STOP;
+bool isRotating = false;
+
+// INTERNET VARIABLES
 const String ssid = "Cornelia 2S";
 const String password = "4tnayti0gd";
+bool isConnected = false;
+int numRetries = 0;
 
-bool isConnected = true;
-
-/*
-struct Button {
-  String wheelSide; // "left" or "right"
-  String direction; // "forward" or "back"
-  boolean state;
-};
-
-const Button buttons[] = {
-  {
-    "left", "forward", 0
-  }, {
-    "left", "back", 0
-  }, {
-    "right", "forward", 0
-  }, {
-    "right", "back", 0
-  }
-};
-*/
-
-const int pins[] = {
-  D0, D1, D2, D3
-};
-
-const int steps[] = {
-  1, 1, 0, 0,
-  0, 1, 1, 0,
-  0, 0, 1, 1,
-  0, 0, 0, 1,
-};
+WebSocketsServer socket(80);
+WiFiClient client;
 
 void setup() {
-  /*
-  Serial.begin(1200);
-  Serial.flush();
-  Serial.println();
-  Serial.println("HERE!");
-  */
-  //connect();
-
-  for (int pin = 0; pin < sizeof(pins); pin++) {
-    /*
-    Serial.print("setting pin ");
-    Serial.print(pin);
-    Serial.print(" to ");
-    Serial.println(OUTPUT);
-    */
-    pinMode(pin, OUTPUT);
-  }
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("START");
+  connect();
+  for (int8_t i = 0; i < 4; i++) pinMode(rightMotorPins[i], OUTPUT);
+  for (int8_t i = 0; i < 4; i++) pinMode(leftMotorPins[i], OUTPUT);
 }
 
 void loop() {
-  //Serial.println("In loop");
-  
-  if (isConnected) {
-    //Serial.println("Taking a step");
-    stepForward();
-  }
-
-
-  // put your main code here, to run repeatedly:
-  /*
-  WiFiClient client = server.available();
-  // wait for a client (web browser) to connect
-  if (client) {
-    Serial.println("\n[Client connected]");
-    while (client.connected()) {
-      // read line by line what the client (web browser) is requesting
-      if (client.available()) {
-        String line = client.readStringUntil('\r');
-        Serial.print(line);
-        // wait for end of client's request, that is marked with an empty line
-        if (line.length() == 1 && line[0] == '\n') {
-          client.println(handleRequest());
-          break;
-        }
-      }
-    }
-
-    while (client.available()) {
-      // but first, let client finish its request
-      // that's diplomatic compliance to protocols
-      // (and otherwise some clients may complain, like curl)
-      // (that is an example, prefer using a proper webserver library)
-      client.read();
-    }
-
-    // close the connection:
-    client.stop();
-    Serial.println("[Client disconnected]");
-  }
-  */
+  socket.loop();
+  setMotorStates();
 }
 
-void stepForward() {
-  for (int step = 0; step < sizeof(steps);) {
-    for (int pin : pins) {
-      digitalWrite(pin, steps[step++]);
-    }
-    delay(5);
+void handleWebSocketEvent(uint8_t num, WStype_t type, uint8_t* payload, size_t length) {
+  if (type == WStype_CONNECTED) {
+    // TODO : Authenticate
   }
+  if (type != WStype_TEXT || length != 2) return;
+  Serial.print("PAYLOAD LEFT: ");
+  Serial.println(payload[LEFT]);
+  Serial.print("PAYLOAD RIGHT: ");
+  Serial.println(payload[RIGHT]);
+  leftMotorState = (int8_t)payload[LEFT];
+  rightMotorState = (int8_t)payload[RIGHT];
 }
-/*
-String handleRequest() {
-  return "HTTP/1.1 204 No Content\r\n";
-}
-
-const int connectInterval = 1000;
-const int connectTimeout = 10000;
-int numRetries = 0;
 
 void connect() {
   WiFi.mode(WIFI_STA);
-  Serial.flush();
   Serial.print("Searching for ");
   Serial.println(ssid);
   Serial.print("Try number ");
@@ -136,22 +90,58 @@ void connect() {
   }
   int elapsedTime = 0;
   while (WiFi.status() != WL_CONNECTED) {
-    delay(connectInterval);
-    elapsedTime += connectInterval;
-    if (elapsedTime >= connectTimeout) {
-      Serial.println("Timeout");
+    delay(CONNECT_INTERVAL);
+    elapsedTime += CONNECT_INTERVAL;
+    if (elapsedTime > CONNECT_TIMEOUT) {
       numRetries++;
       return connect();
     }
-    Serial.print("Connecting...");
-    Serial.println(WiFi.status());
-    Serial.println(elapsedTime);
-    Serial.flush();
   }
   Serial.println("Connected");
-
-  //server.begin();
+    
+  socket.begin();
+  socket.onEvent(handleWebSocketEvent);
 
   isConnected = true;
 }
-*/
+
+void setMotorStates() {
+  setMotorState(LEFT, leftMotorState);
+  setMotorState(RIGHT, rightMotorState);
+}
+
+void setMotorState(int8_t side, int8_t state) {
+  if (state == STOP) return;
+  Serial.print("Setting motor state: ");
+  Serial.print(side);
+  Serial.println(state);
+  if (state == FORWARD) clockwiseStep(side);
+  else if (state == REVERSE) ctrclockwiseStep(side);
+}
+
+void clockwiseStep(int8_t side) {
+  isRotating = true;
+  for (int8_t i = 0; i < sizeof(stepSequence);) {
+    uint8_t step[4] = {stepSequence[i++], stepSequence[i++], stepSequence[i++], stepSequence[i++]};
+    writeMotorPins(side, step);
+    int startTime = millis();
+    while (startTime + STEP_DELAY >= millis());
+  }
+  isRotating = false;
+}
+
+void ctrclockwiseStep(int8_t side) {
+  isRotating = true;
+  for (int8_t i = sizeof(stepSequence); i >= 0; i-=4) {
+    uint8_t step[4] = {stepSequence[i-4], stepSequence[i-3], stepSequence[i-2], stepSequence[i-1]};
+    writeMotorPins(side, step);
+    int startTime = millis();
+    while (startTime + STEP_DELAY >= millis());
+  }
+  isRotating = false;
+}
+
+void writeMotorPins(int8_t side, uint8_t values[4]) {
+  if (side == LEFT) for (int8_t i = 0; i < 4; i++) digitalWrite(leftMotorPins[i], values[i]);
+  else if (side == RIGHT) for (int8_t i = 0; i < 4; i++) digitalWrite(rightMotorPins[i], values[i]);
+}
